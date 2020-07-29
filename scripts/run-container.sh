b0aecc32feaa287a10f18437a7731820b33ba373
andygeiss <4ndygeiss@gmail.com>
2020-07-29 14:10:35 +0200

    install addded

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Ensure NUMA is enabled
-for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node > /dev/null; done
+for a in /sys/bus/pci/devices/*; do echo 0 | sudo tee -a $a/numa_node >/dev/null; done
 
 docker run -it \
   -e "GOPATH=${GOPATH}" \
@@ -9,7 +9,7 @@ docker run -it \
   -u $(id -u):$(id -g) \
   -v "${GOPATH}":"${GOPATH}" \
   -w "${PWD}/project" \
-  --runtime=nvidia \
+  --runtime nvidia \
   --rm \
   --name machine-learning-golang machine-learning-golang:latest \
   bash
